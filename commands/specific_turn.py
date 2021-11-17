a0208277a69af2223c4ab5a53cb63477d76e34c9
Thomas <4001137@keranna.qc.ca>
2021-11-17 17:25:53 -0500

    Crée cmd tourner angle specifique et cmd reset odometry

---
+++ commands/specific_turn.py
@@ -0,0 +1,22 @@
+import commands2
+from subsystems.basepilotable import BasePilotable
+
+class SpecificTurn(commands2.CommandBase):
+    def __init__(self, angle: float, drive: BasePilotable, turn_speed: float):
+        super().__init__()
+        self.angle = angle
+        self.drive = drive
+        self.turn_speed = turn_speed
+        self.addRequirements(self.drive)
+
+    def initialize(self):
+        self.drive.resetOdometry()
+
+    def execute(self):
+        self.drive.driveCartesian(0, 0, self.turn_speed)
+
+    def end(self, interrupted: bool):
+        self.drive.driveCartesian(0, 0, 0)
+
+    def isFinished(self) -> bool:
+        return self.drive.gyro.getAngle() >= self.angle
--- subsystems/basepilotable.py
+++ subsystems/basepilotable.py
@@ -24,7 +24,7 @@ class BasePilotable(commands2.SubsystemBase):
         self.rl_motor_encoder = wpilib.Encoder(*constants.Ports.base_pilotable_encodeur_rl)
         self.rr_motor_encoder = wpilib.Encoder(*constants.Ports.base_pilotable_encodeur_rr)
 
-        self.gyro = wpilib.AnalogGyro(constants.Ports.base_pilotable_gyro)
+        self.gyro = wpilib.ADXRS450_Gyro()
 
         self.drive = wpilib.drive.MecanumDrive(self.fl_motor, self.fr_motor, self.rl_motor,self.rr_motor)
 
@@ -32,7 +32,7 @@ class BasePilotable(commands2.SubsystemBase):
         self.fr_motor_encoder.setDistancePerPulse(1 / self.pulses_per_meter)
         self.rl_motor_encoder.setDistancePerPulse(1 / self.pulses_per_meter)
         self.rr_motor_encoder.setDistancePerPulse(1 / self.pulses_per_meter)
-        self.resetEncoders()
+        self.resetOdometry()
 
     def driveCartesian(self, ySpeed: float, xSpeed: float, zRot: float) -> None:
         self.drive.driveCartesian(ySpeed, xSpeed, zRot, self.gyro.getAngle())
@@ -40,14 +40,13 @@ class BasePilotable(commands2.SubsystemBase):
     def drivePolar(self, mag: float, angle: float, zRot: float) -> None:
         self.drive.drivePolar(mag, angle, zRot)
 
-    def resetEncoders(self) -> None:
+    def resetOdometry(self) -> None:
         self.fl_motor_encoder.reset()
         self.fr_motor_encoder.reset()
         self.rl_motor_encoder.reset()
         self.rr_motor_encoder.reset()
-
-    def resetGyro(self) -> None:
         self.gyro.reset()
+
     
     def getAverageEncoderDistance(self) -> float:
         return (self.fl_motor_encoder.getDistance() + 
