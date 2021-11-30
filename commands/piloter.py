c490ebc979de1af6ec12e067a9c073cf710191d6
Ultime 5528 <keranna.first@gmail.com>
2021-11-29 19:01:49 -0500

    RestoreFactoryDefaults

--- commands/piloter.py
+++ commands/piloter.py
@@ -7,7 +7,6 @@ from subsystems.basepilotable import BasePilotable
 class Piloter(CommandBase):
     def __init__(self, base_pilotable: BasePilotable, controller: wpilib.Joystick):
         super().__init__()
-
         self.controller = controller
         self.base_pilotable = base_pilotable
         self.addRequirements(base_pilotable)
@@ -17,3 +16,4 @@ class Piloter(CommandBase):
         self.base_pilotable.driveCartesian(
             .5*self.controller.getX(), -0.5*self.controller.getY(), 0*self.controller.getZ()
         )
+        # self.base_pilotable.drive_test(-0.5 * self.controller.getY())
