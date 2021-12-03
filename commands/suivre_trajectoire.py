8d24079d88419ede727b20124cd4ebd5768a9f78
Ultime 5528 <keranna.first@gmail.com>
2021-12-03 15:06:52 -0500

    Corrections SuivreTrajectoire

--- commands/suivre_trajectoire.py
+++ commands/suivre_trajectoire.py
@@ -4,12 +4,14 @@ import commands2
 from subsystems.basepilotable import BasePilotable
 from wpimath.geometry import Pose2d
 from termcolor import colored
+import math
 
 
 class SuivreTrajectoire(commands2.CommandBase):
     def __init__(self, drive: BasePilotable, end_position: Pose2d) -> None:
         super().__init__()
         self.drive = drive
+        self.addRequirements(self.drive)
         self.end_position = end_position
 
     def initialize(self) -> None:
@@ -19,11 +21,14 @@ class SuivreTrajectoire(commands2.CommandBase):
         twist = self.drive.odometry.getPose().log(self.end_position)
         hypotenuse = math.hypot(twist.dx, twist.dy)
         print(colored(twist.dx/hypotenuse, "green"))
-        self.drive.driveCartesian(twist.dy / hypotenuse, twist.dx / hypotenuse, twist.dtheta_degrees)
+        speed = 0.4
+        turn_speed = 0.05
+        self.drive.driveCartesian(-speed * twist.dy / hypotenuse, speed * twist.dx / hypotenuse, math.copysign(turn_speed, -twist.dtheta_degrees))
 
     def isFinished(self) -> bool:
         print(colored(self.drive.odometry.getPose().translation().distance(self.end_position.translation()), "yellow"))
-        return self.drive.odometry.getPose().translation().distance(self.end_position.translation()) <= 0.25
+        distance = self.drive.odometry.getPose().translation().distance(self.end_position.translation())
+        return distance <= 0.25
 
     def end(self, interrupted: bool) -> None:
         print(colored("Finish", "red"))
